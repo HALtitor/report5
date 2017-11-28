35f05e34f8165352860d5c41c2b257899c04a55d
Yamakawa Haruki <e175753@ie.u-ryukyu.ac.jp>
2017-11-28 14:38:32 +0900

    first commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e175753/Hero.java
@@ -0,0 +1,24 @@
+package jp.ac.uryukyu.ie.e175753;
+
+public class Hero extends LivingThing{
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name ヒーロー名
+     * @param maximumHP ヒーローのHP
+     * @param attack ヒーローの攻撃力
+     */
+    public Hero (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    int HP=getHitPoint();
+    @Override
+    public void wounded(int damage) {
+        HP -= damage;
+        if (HP < 0) {
+            setDead(true);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
+        }
+    }
+}
